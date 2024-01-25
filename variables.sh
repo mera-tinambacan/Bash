08177068127d8f1c0f236869a07ab84748950754
Meracle Tinambacan <meracle.tinambacan@awsys-i.com>
2024-01-25 12:49:29 +0800

    updated variable file

--- variables.sh
+++ variables.sh
@@ -8,3 +8,15 @@
  echo "$greeting, $name"
 
 ## Result: Good morning, Mera
+
+# -- Assigning variables thru user input --
+
+#!/bin/bash
+
+echo "Enter two numbers:"
+read -p "Number 1: " num1
+read -p "Number 2: " num2
+
+echo "The sum of $num1 and $num2 is $(($num1 + $num2))"
+
+# Result: the sum of num1 and num2 is numm1+num2
