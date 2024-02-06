cd52870d067a9404bb205a40d85e12f072367611
Meracle Tinambacan <meracle.tinambacan@awsys-i.com>
2024-02-06 15:01:55 +0800

    ftp-aws2 -list all directories

---
+++ ftp-aws2.py
@@ -0,0 +1,23 @@
+from ftplib import FTP
+
+def lambda_handler(event, context):
+    ftp_host = "13.211.18.29"
+    ftp_user = "aws"
+    ftp_password = "aws"
+    target_directory = "/home/ubuntu"
+
+    try:
+        with FTP(ftp_host, ftp_user, ftp_password) as ftp:
+            print("Successfully connected to FTP server.")
+            
+            # Change working directory to the target directory
+            ftp.cwd(target_directory)
+            
+            # Get a list of directories
+            directories = ftp.nlst()
+            print("Directories in", target_directory, ":")
+            for directory in directories:
+                print(directory)
+            
+    except Exception as e:
+        print(f"Error: {e}")
