34eed19773e60fbf97be3b9983579e937449cb09
Meracle Tinambacan <meracle.tinambacan@awsys-i.com>
2024-03-18 16:35:03 +0800

    Update logLevels.sh

--- AWS-IPA/logLevels.sh
+++ AWS-IPA/logLevels.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Source the parameter function
+source parameter_function.sh
+
 # FTPS server details
 ftps_server="ftp://localhost"
 ftps_file="destination4.csv"
@@ -14,17 +17,11 @@ log_stream_name="ipaFtp-stream"
 # Temporary directory to store the downloaded file
 temp_dir="/tmp"
 
-# Fetch parameter value from AWS Systems Manager Parameter Store
-get_parameter() {
-  local name="$1"
-  aws ssm get-parameter --name "$name" --query "Parameter.Value" --output text
-}
-
 # Function to put logs to CloudWatch
 put_logs() {
   local log_level="$1"
   local message="$2"
-  local timestamp=$(date +%s%3N) # Get current timestamp in milliseconds
+  local timestamp=$(date +%s%3N)
   
   # Construct the JSON object for the log event
   local log_event="{\"timestamp\": $timestamp, \"message\": \"$log_level - $message\"}"
@@ -44,21 +41,20 @@ transfer() {
   username=$(get_parameter "/ftp/username")
   password=$(get_parameter "/ftp/password")
 
-  put_logs "INFO" "Downloading $ftps_file from FTPS..."
+  put_logs "[INFO] Downloading $ftps_file from FTPS server"
   curl -k --ftp-ssl --user "$username:$password" "$ftps_server/$ftps_file" -o "$temp_dir/$ftps_file"
   if [ $? -eq 0 ]; then
-    put_logs "INFO" "Download complete."
+    put_logs "[INFO] Download complete."
     local_file="$temp_dir/$ftps_file"
-    put_logs "INFO" "Transferring $local_file to S3..."
+    put_logs "[INFO] Transferring $local_file to S3 $s3_bucket bucket."
     aws s3 cp "$local_file" "s3://$s3_bucket"
-    put_logs "INFO" "Transfer complete."
-    put_logs "INFO" "Deleting $ftps_file from FTPS server..."
+    put_logs "[INFO] Transfer complete."
+    put_logs "[INFO] Deleting $ftps_file from FTPS server"
     curl -k --ftp-ssl --user "$username:$password" -Q "DELE $ftps_file" "$ftps_server"
-    put_logs "INFO" "Deletion complete."
+    put_logs "[INFO] Deletion complete."
   else
-    put_logs "ERROR" "Failed to download $ftps_file from FTPS."
+    put_logs "[ERROR] Failed to download $ftps_file from FTPS."
   fi
 }
 
-# Run the transfer function
 transfer
