fa77571d9c7a8414c76734af7a21a05642a38ef4
Meracle Tinambacan <meracle.tinambacan@awsys-i.com>
2024-03-19 15:30:03 +0800

    Update logLevels.sh

--- AWS-IPA/logLevels.sh
+++ AWS-IPA/logLevels.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# Source the parameter function
+# Source the parameter functions
 source parameter_function.sh
 
 # FTPS server details
 ftps_server="ftp://localhost"
-ftps_file="destination4.csv"
+ftps_file="source.csv"
 
 # AWS S3 details
 s3_bucket="mydestination-directory"
 
 # CloudWatch Logs details
 log_group_name="ipaFtp-Log"
-log_stream_name="ipaFtp-stream"
+log_stream_name="ipaFtp-Stream"
 
 # Temporary directory to store the downloaded file
 temp_dir="/tmp"
 
-# Function to put logs to CloudWatch
+# Function to put logs to Cloudwatch
 put_logs() {
   local log_level="$1"
   local message="$2"
   local timestamp=$(date +%s%3N)
-  
+
   # Construct the JSON object for the log event
   local log_event="{\"timestamp\": $timestamp, \"message\": \"$log_level - $message\"}"
 
@@ -37,19 +37,19 @@ transfer() {
   local username
   local password
 
-  # Fetch FTP username and password from AWS Systems Manager Parameter Store
+  # Fetch FTP username and password from source
   username=$(get_parameter "/ftp/username")
   password=$(get_parameter "/ftp/password")
 
-  put_logs "[INFO] Downloading $ftps_file from FTPS server"
+  put_logs "Downloading $ftps_file from FTPS..."
   curl -k --ftp-ssl --user "$username:$password" "$ftps_server/$ftps_file" -o "$temp_dir/$ftps_file"
   if [ $? -eq 0 ]; then
     put_logs "[INFO] Download complete."
     local_file="$temp_dir/$ftps_file"
-    put_logs "[INFO] Transferring $local_file to S3 $s3_bucket bucket."
+    put_logs "[INFO] Transferring $local_file to S3..."
     aws s3 cp "$local_file" "s3://$s3_bucket"
     put_logs "[INFO] Transfer complete."
-    put_logs "[INFO] Deleting $ftps_file from FTPS server"
+    put_logs "[INFO] Deleting $ftps_file from FTPS server..."
     curl -k --ftp-ssl --user "$username:$password" -Q "DELE $ftps_file" "$ftps_server"
     put_logs "[INFO] Deletion complete."
   else
