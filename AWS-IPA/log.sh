d787294fd3e3fd5f95cafa4122ab2af689def39b
Meracle Tinambacan <meracle.tinambacan@awsys-i.com>
2024-04-05 11:04:44 +0800

    Create log.sh

---
+++ AWS-IPA/log.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LOG_GROUP_NAME="HostToIPA"
+LOG_STREAM_NAME="RHEL6-HostToIPA-Stream"
+LOG_FILE="file2.log"
+
+# Function to send log event to CloudWatch Logs
+send_log_event() {
+    local log_event=$1
+    aws logs put-log-events \
+        --log-group-name "$LOG_GROUP_NAME" \
+        --log-stream-name "$LOG_STREAM_NAME" \
+        --log-events "$log_event"
+}
+
+# Send each line to CloudWatch Logs
+while IFS= read -r line; do
+    timestamp=$(date -u +%s%3N)
+    # Escape double quotes in the log message and convert to JSON
+    log_event=$(jq -n --argjson ts "$timestamp" --arg msg "$line" '{"timestamp":$ts|tonumber, "message":$msg}')
+    # Send log event to CloudWatch Logs
+    send_log_event "$log_event"
+done < "$LOG_FILE"
+
+#cat tm1server.log | tr -s " " | cut -d" " -f3- | cut -d" " -f4 --complement | tail > file2.log
+#grep "successful" tm1server.log | tr -s " " | cut -d" " -f3- | cut -d" " -f4 --complement > file4.log
